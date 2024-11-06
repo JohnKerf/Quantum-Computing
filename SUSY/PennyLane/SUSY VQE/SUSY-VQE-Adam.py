c20013a61a9f4692a259e575797e5b51c7ac4122
JohnKerf <johnkerfoot234@gmail.com>
2024-11-06 09:54:09 +0000

    Run Adam for varying tolerance - No shot noise

--- SUSY/PennyLane/SUSY VQE/SUSY-VQE-Adam.py
+++ SUSY/PennyLane/SUSY VQE/SUSY-VQE-Adam.py
@@ -13,22 +13,23 @@ import matplotlib.pyplot as plt
 from qiskit.quantum_info import SparsePauliOp
 
 #custom module
-from susy_qm import calculate_Hamiltonian
+from susy_qm import calculate_Hamiltonian, create_plots
 
 
 #potential = 'QHO'
 potential = 'AHO'
 #potential = 'DW'
 
-cut_offs_list = [2,4,8]#,16,32]
+cut_offs_list = [2,4,8,16]#,32]
 tol_list = [1e-2, 1e-4, 1e-6, 1e-8]
-tol_list = [1e-6]
+#tol_list = [1e-6]
 
 for tolerance in tol_list:
 
     starttime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    folder = str(starttime)
     #Create directory for files
-    os.makedirs(r"C:\Users\Johnk\OneDrive\Desktop\PhD 2024\Quantum Computing Code\Quantum-Computing\PennyLane\SUSY VQE\Adam\Files\{}\\{}".format(potential, str(starttime)))
+    os.makedirs(r"C:\Users\Johnk\OneDrive\Desktop\PhD 2024\Quantum Computing Code\Quantum-Computing\SUSY\PennyLane\SUSY VQE\No Shot Noise\Adam\Files\{}\\{}".format(potential, folder))
 
     print(f"Running for {potential} potential")
 
@@ -48,8 +49,9 @@ for tolerance in tol_list:
         num_qubits = hamiltonian.num_qubits
 
         # Device
+        shots = None
         #dev = qml.device('default.qubit', wires=num_qubits, shots=100)
-        dev = qml.device('lightning.qubit', wires=num_qubits, shots=100)
+        dev = qml.device('lightning.qubit', wires=num_qubits, shots=shots)
 
         # Define the cost function
         @qml.qnode(dev)
@@ -170,61 +172,12 @@ for tolerance in tol_list:
         }
 
         # Save the variable to a JSON file
-        path = r"C:\Users\Johnk\OneDrive\Desktop\PhD 2024\Quantum Computing Code\Quantum-Computing\PennyLane\SUSY VQE\Adam\Files\{}\\{}\{}_{}.json".format(potential, str(starttime), potential, cut_off)
+        path = r"C:\Users\Johnk\OneDrive\Desktop\PhD 2024\Quantum Computing Code\Quantum-Computing\SUSY\PennyLane\SUSY VQE\No Shot Noise\Adam\Files\{}\\{}\{}_{}.json".format(potential, folder, potential, cut_off)
         with open(path, 'w') as json_file:
             json.dump(run, json_file, indent=4)
 
 
-    # Load all data and create graphs
-    print("Creating plots")
-    data_dict = {}
-    base_path = r"C:\Users\Johnk\OneDrive\Desktop\PhD 2024\Quantum Computing Code\Quantum-Computing\PennyLane\SUSY VQE\Adam\Files\{}\\{}\{}_{}.json"
-
-    for n in cut_offs_list:
-        file_path = base_path.format(potential, str(starttime), potential, n)
-        with open(file_path, 'r') as json_file:
-            data_dict[f'c{n}'] = json.load(json_file)
-
-
-    #Create and save plots
-    num_cutoffs = len(cut_offs_list)
-    nrows = int(np.ceil(num_cutoffs/2))
-    fig, axes = plt.subplots(nrows=nrows, ncols=2, figsize=(30, 5*nrows))
-    axes = axes.flatten()
-
-    for idx, (cutoff, cutoff_data) in enumerate(data_dict.items()):
-        
-        results = cutoff_data['results']
-        x_values = range(len(results))
-
-        # Calculating statistics
-        mean_value = np.mean(results)
-        median_value = np.median(results)
-        min_value = np.min(results)
-
-        # Creating the plot
-        ax = axes[idx]
-        ax.plot(x_values, results, marker='o', label='Energy Results')
-
-        # Plot mean, median, and min lines
-        ax.axhline(y=mean_value, color='r', linestyle='--', label=f'Mean = {mean_value:.6f}')
-        ax.axhline(y=median_value, color='g', linestyle='-', label=f'Median = {median_value:.6f}')
-        ax.axhline(y=min_value, color='b', linestyle='-.', label=f'Min = {min_value:.6f}')
-
-        ax.set_ylim(min_value - 0.01, max(results) + 0.01)
-        ax.set_xlabel('Run')
-        ax.set_ylabel('Ground State Energy')
-        ax.set_title(f"{potential}: Cutoff = {cutoff_data['cutoff']}")
-        ax.legend()
-        ax.grid(True)
-
-    # Hide any remaining unused axes
-    for idx in range(num_cutoffs, len(axes)):
-        fig.delaxes(axes[idx])
-
-    print("Saving plots")
-    plt.tight_layout()
-    plt.savefig(r"C:\Users\Johnk\OneDrive\Desktop\PhD 2024\Quantum Computing Code\Quantum-Computing\PennyLane\SUSY VQE\Adam\Files\{}\\{}\results.png".format(potential, str(starttime)))
-
-    print("Done")
-
+    # create plots
+    base_path = r"C:\Users\Johnk\OneDrive\Desktop\PhD 2024\Quantum Computing Code\Quantum-Computing\SUSY\PennyLane\SUSY VQE\No Shot Noise\Adam\Files\{}\\{}\\"
+    create_plots(potential=potential, base_path=base_path, folder=folder, cut_off_list=cut_offs_list)
+   
