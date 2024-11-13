5dd08c9709835018b96b6efc0e6cb07d29ed2f7e
JohnKerf <johnkerfoot234@gmail.com>
2024-11-13 12:45:52 +0000

    rearrange susy_qm package folders

--- SUSY/susy_qm/setup.py
+++ SUSY/susy_qm/setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup#, find_packages
 
 setup(
     name="susy_qm",
     version="1.0",
-    packages=find_packages(),
+    packages=['susy_qm'],
     install_requires=[],  # Add any dependencies here if needed
 )
