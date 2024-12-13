a7e7fb89598b896101057462db59efe43dedbe68
JohnKerf <johnkerfoot234@gmail.com>
2024-12-13 10:08:05 +0000

    Add logic to only include converge data

--- packages/susy_qm/susy_qm/plots.py
+++ packages/susy_qm/susy_qm/plots.py
@@ -2,8 +2,9 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 
-def create_vqe_plots(potential, base_path, cut_off_list, individual):
-    # Load all data and create graphs
+def create_vqe_plots(potential, base_path, cut_off_list, individual, converged_only=False):
+
+    # Load all data from folder
     print("Creating plots")
     data_dict = {}
 
@@ -12,17 +13,24 @@ def create_vqe_plots(potential, base_path, cut_off_list, individual):
         with open(file_path, 'r') as json_file:
             data_dict[f'c{n}'] = json.load(json_file)
 
+    if not individual:
+    # Create axes for plots
+        num_cutoffs = len(cut_off_list)
+        nrows = int(np.ceil(num_cutoffs/2))
+        fig, axes = plt.subplots(nrows=nrows, ncols=2, figsize=(30, 5*nrows))
+        axes = axes.flatten()
 
-    #Create and save plots
-    num_cutoffs = len(cut_off_list)
-    nrows = int(np.ceil(num_cutoffs/2))
-    fig, axes = plt.subplots(nrows=nrows, ncols=2, figsize=(30, 5*nrows))
-    axes = axes.flatten()
+    for idx, (_, data) in enumerate(data_dict.items()):
 
-    for idx, (cutoff, cutoff_data) in enumerate(data_dict.items()):
+        # Remove data that didnt converge if required
+        if converged_only:
+            successful_indices = [i for i, success in enumerate(data['success']) if success]
+            results = [data['results'][i] for i in successful_indices]
+        else:
+            results = data['results']
         
-        results = cutoff_data['results']
-        exact = np.min(cutoff_data['exact_eigenvalues'])
+        cutoff = data['cutoff']
+        exact = np.min(data['exact_eigenvalues'])
         x_values = range(len(results))
 
         # Calculating statistics
@@ -30,49 +38,56 @@ def create_vqe_plots(potential, base_path, cut_off_list, individual):
         median_value = np.median(results)
         min_value = np.min(results)
 
-        # Creating the plot
-        ax = axes[idx]
-        ax.plot(x_values, results, marker='o', label='Energy Results')
-
-        # Plot mean, median, and min lines
-        ax.axhline(y=mean_value, color='r', linestyle='--', label=f'Mean = {mean_value:.6f}')
-        ax.axhline(y=median_value, color='g', linestyle='-', label=f'Median = {median_value:.6f}')
-        ax.axhline(y=min_value, color='b', linestyle='-.', label=f'Min = {min_value:.6f}')
-        ax.axhline(y=exact, color='orange', linestyle=':', label=f'Exact = {exact:.6f}')
-
-        ax.set_ylim(min_value - 0.01, max(results) + 0.01)
-        ax.set_xlabel('Run')
-        ax.set_ylabel('Ground State Energy')
-        ax.set_title(f"{potential}: Cutoff = {cutoff_data['cutoff']}")
-        ax.legend()
-        ax.grid(True)
-
-
         if individual:
 
             # Save individual plot
-            print(f"Saving individual plot for cutoff {cutoff_data['cutoff']}")
+            print(f"Saving individual plot for cutoff {cutoff}")
 
-            plt.figure()
+            plt.figure(figsize=(15,10))
             plt.plot(x_values, results, marker='o', label='Energy Results')
+
+            # Plot mean, median, min and exact lines
             plt.axhline(y=mean_value, color='r', linestyle='--', label=f'Mean = {mean_value:.6f}')
             plt.axhline(y=median_value, color='g', linestyle='-', label=f'Median = {median_value:.6f}')
             plt.axhline(y=min_value, color='b', linestyle='-.', label=f'Min = {min_value:.6f}')
             plt.axhline(y=exact, color='orange', linestyle=':', label=f'Exact = {exact:.6f}')
+
             plt.ylim(min_value - 0.01, max(results) + 0.01)
             plt.xlabel('Run')
             plt.ylabel('Ground State Energy')
-            plt.title(f"{potential}: Cutoff = {cutoff_data['cutoff']}")
+            plt.title(f"{potential}: Cutoff = {cutoff}")
             plt.legend()
             plt.grid(True)
-            plt.savefig(base_path.format(potential) + f"cutoff_{cutoff_data['cutoff']}_plot.png")
+            plt.savefig(base_path.format(potential) + f"cutoff_{cutoff}_plot.png")
             plt.close()
 
-    # Hide any remaining unused axes
-    for idx in range(num_cutoffs, len(axes)):
-        fig.delaxes(axes[idx])
+        else:
+
+            # Add each plot to same figure
+            ax = axes[idx]
+            ax.plot(x_values, results, marker='o', label='Energy Results')
+
+            # Plot mean, median, min and exact lines
+            ax.axhline(y=mean_value, color='r', linestyle='--', label=f'Mean = {mean_value:.6f}')
+            ax.axhline(y=median_value, color='g', linestyle='-', label=f'Median = {median_value:.6f}')
+            ax.axhline(y=min_value, color='b', linestyle='-.', label=f'Min = {min_value:.6f}')
+            ax.axhline(y=exact, color='orange', linestyle=':', label=f'Exact = {exact:.6f}')
+
+            ax.set_ylim(min_value - 0.01, max(results) + 0.01)
+            ax.set_xlabel('Run')
+            ax.set_ylabel('Ground State Energy')
+            ax.set_title(f"{potential}: Cutoff = {cutoff}")
+            ax.legend()
+            ax.grid(True)
+
+    
 
     if not individual:
+
+        # Hide any remaining unused axes
+        for idx in range(num_cutoffs, len(axes)):
+            fig.delaxes(axes[idx])
+
         print("Saving combined plots")
         plt.tight_layout()
         plt.savefig(base_path.format(potential) + "results.png")
