eb2a2d3dbb78a973817a5de4563b444d76b980c0
JohnKerf <156371414+JohnKerf@users.noreply.github.com>
2025-03-27 16:59:55 +0000

    Run ADAPT-VQE

--- SUSY/SUSY QM/PennyLane/ADAPT-VQE/ADAPT-VQE.py
+++ SUSY/SUSY QM/PennyLane/ADAPT-VQE/ADAPT-VQE.py
@@ -30,17 +30,11 @@ def compute_grad(param, H, num_qubits, operator_ham, op_list, op_params, basis_s
         param_index = 0
         for op in op_list:
             o = type(op)
-            if (o == qml.CNOT) or (o == qml.CZ):
-                o(wires=op.wires)
-            else:
-                o(op_params[param_index], wires=op.wires)
-                param_index +=1
+            o(op_params[param_index], wires=op.wires)
+            param_index +=1
 
         oph = type(operator_ham)
-        if (oph == qml.CNOT) or (oph == qml.CZ):
-            oph(wires=operator_ham.wires)
-        else:
-            oph(param, wires=operator_ham.wires)
+        oph(param, wires=operator_ham.wires)
 
         return qml.expval(qml.Hermitian(H, wires=range(num_qubits)))
     
@@ -65,11 +59,8 @@ def cost_function(params, H, num_qubits, shots, op_list, basis_state):
         param_index = 0
         for op in op_list:
             o = type(op)
-            if (o == qml.CNOT) or (o == qml.CZ):
-                o(wires=op.wires)
-            else:
-                o(params[param_index], wires=op.wires)
-                param_index +=1
+            o(params[param_index], wires=op.wires)
+            param_index +=1
 
         return qml.expval(qml.Hermitian(H, wires=range(num_qubits)))
 
@@ -80,14 +71,14 @@ def cost_function(params, H, num_qubits, shots, op_list, basis_state):
     return circuit(params), device_time
 
 
-def run_adapt_vqe(i, max_iter, tol, abs_tol, strategy, popsize, H, num_qubits, shots, num_steps, phi, num_grad_checks, operator_pool, basis_state):
+def run_adapt_vqe(i, max_iter, tol, abs_tol, strategy, popsize, H, num_qubits, shots, num_steps, phi, num_grad_checks, operator_pool, basis_state, min_eigenvalue):
 
     # We need to generate a random seed for each process otherwise each parallelised run will have the same result
     seed = (os.getpid() * int(time.time())) % 123456789
     run_start = datetime.now()
 
     # Generate Halton sequence
-    num_dimensions = 2#2*num_qubits#np.prod(params_shape)
+    num_dimensions = 2
     num_samples = popsize
     halton_sampler = Halton(d=num_dimensions, seed=seed)
     halton_samples = halton_sampler.random(n=num_samples)
@@ -105,6 +96,7 @@ def run_adapt_vqe(i, max_iter, tol, abs_tol, strategy, popsize, H, num_qubits, s
     # Main ADAPT-VQE script
     op_list = []
     op_params = []
+    energies = []
 
     pool = operator_pool.copy()
     success = False
@@ -117,7 +109,7 @@ def run_adapt_vqe(i, max_iter, tol, abs_tol, strategy, popsize, H, num_qubits, s
             #print(f"Removing {most_common_gate} from pool")
             pool.remove(most_common_gate)
 
-            if type(most_common_gate) == qml.CRX:
+            if (type(most_common_gate) == qml.CRX) or (type(most_common_gate) == qml.CRY):
                 cq = most_common_gate.wires[0]
                 tq = most_common_gate.wires[1]
 
@@ -133,12 +125,9 @@ def run_adapt_vqe(i, max_iter, tol, abs_tol, strategy, popsize, H, num_qubits, s
             grad_list = []
             for op in pool:
                 grad = compute_grad(param, H, num_qubits, op, op_list, op_params, basis_state)
-                o=type(op)
 
-                if (o == qml.CNOT) or (o == qml.CZ):
-                    grad_op = o(wires=op.wires)
-                else:
-                    grad_op = o(param, wires=op.wires)
+                o=type(op)
+                grad_op = o(param, wires=op.wires)
 
                 grad_list.append((grad_op,abs(grad)))
 
@@ -178,14 +167,19 @@ def run_adapt_vqe(i, max_iter, tol, abs_tol, strategy, popsize, H, num_qubits, s
         pre_op_params = op_params
         op_params = res.x
 
+        energies.append(min_e)
         #print(f"Min E: {min_e}")
         #print(res.success)
 
         if i!=0:
             if abs(pre_min_e - min_e) < 1e-8:
                 #print("gradient converged")
+                energies.pop()
                 op_list.pop()
                 op_params = pre_op_params.tolist().pop()
+                success = True
+                break
+            if abs(min_eigenvalue-min_e) < 1e-6:
                 success = True
                 break
 
@@ -194,6 +188,7 @@ def run_adapt_vqe(i, max_iter, tol, abs_tol, strategy, popsize, H, num_qubits, s
 
     return {
         "seed": seed,
+        "energies": energies,
         "min_energy": min_e,
         "op_params": op_params,
         "op_list": op_list,
@@ -206,14 +201,14 @@ def run_adapt_vqe(i, max_iter, tol, abs_tol, strategy, popsize, H, num_qubits, s
 
 if __name__ == "__main__":
     
-    potential = "QHO"
-    cutoff = 8
+    potential = "AHO"
+    cutoff = 32
     shots = 1024
 
     print(f"Running for {potential} potential, cutoff {cutoff}")
 
     starttime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    base_path = os.path.join(r"C:\Users\Johnk\Documents\PhD\Quantum Computing Code\Quantum-Computing\SUSY\SUSY QM\PennyLane\ADAPT-VQE\Files", potential)
+    base_path = os.path.join(r"C:\Users\Johnk\Documents\PhD\Quantum Computing Code\Quantum-Computing\SUSY\SUSY QM\PennyLane\ADAPT-VQE\Files\Files", potential, str(starttime))
     os.makedirs(base_path, exist_ok=True)
 
 
@@ -231,36 +226,37 @@ if __name__ == "__main__":
 
     #Create operator pool
     operator_pool = []
-    phi = np.pi/2
+    phi = 0.0#np.pi/2
     for i in range(num_qubits):
         operator_pool.append(qml.RY(phi,wires=[i]))
         operator_pool.append(qml.RZ(phi,wires=[i]))
-        #operator_pool.append(qml.RX(p,wires=[i]))
+        operator_pool.append(qml.RX(phi,wires=[i]))
 
     c_pool = []
 
     for control in range(num_qubits):
             for target in range(num_qubits):
                 if control != target:
-                    #c_pool.append(qml.CZ(wires=[control, target]))
-                    #c_pool.append(qml.CNOT(wires=[control, target]))
-                    #operator_pool.append(qml.IsingXX(p,wires=[control, target]))
-                    #c_pool.append(qml.CRY(phi=np.pi/2, wires=[control, target]))
+                    c_pool.append(qml.CRY(phi=phi, wires=[control, target]))
                     c_pool.append(qml.CRX(phi=phi, wires=[control, target]))
 
     operator_pool = operator_pool + c_pool    
 
     # Choose basis state
-    basis_state = [1] + [0]*(num_qubits-1)
+    if potential == 'DW':
+        basis_state = [0]*(num_qubits)
+    else:
+        basis_state = [1] + [0]*(num_qubits-1)
+    
 
     # Optimizer
-    num_steps = 2
-    num_grad_checks = 1
-    num_vqe_runs = 2
+    num_steps = 10
+    num_grad_checks = 10
+    num_vqe_runs = 40
     max_iter = 10000
     strategy = "randtobest1bin"
     tol = 1e-3
-    abs_tol = 1e-3
+    abs_tol = 1e-2
     popsize = 20
 
     vqe_starttime = datetime.now()
@@ -271,7 +267,7 @@ if __name__ == "__main__":
         vqe_results = pool.starmap(
             run_adapt_vqe,
             [
-                (i, max_iter, tol, abs_tol, strategy, popsize, H, num_qubits, shots, num_steps, phi, num_grad_checks, operator_pool, basis_state)
+                (i, max_iter, tol, abs_tol, strategy, popsize, H, num_qubits, shots, num_steps, phi, num_grad_checks, operator_pool, basis_state, min_eigenvalue)
                 for i in range(num_vqe_runs)
             ],
         )
@@ -279,7 +275,8 @@ if __name__ == "__main__":
     print("Finished ADAPT-VQE")
     # Collect results
     seeds = [res["seed"] for res in vqe_results]
-    energies = [res["min_energy"] for res in vqe_results]
+    all_energies = [res["energies"] for res in vqe_results]
+    min_energies = [res["min_energy"] for res in vqe_results]
     op_params = [str(res["op_params"]) for res in vqe_results]
     op_lists = [str(res["op_list"]) for res in vqe_results]
     success = [res["success"] for res in vqe_results]
@@ -314,8 +311,10 @@ if __name__ == "__main__":
         "num_steps":num_steps,
         "num_grad_checks":num_grad_checks,
         "phi": phi,
+        "basis_state": basis_state,
         "operator_pool": [str(op) for op in operator_pool],
-        "min_energies": energies,
+        "all_energies": all_energies,
+        "min_energies": min_energies,
         "op_params": op_params,
         "op_list": op_lists,
         "num_iters": num_iters,
