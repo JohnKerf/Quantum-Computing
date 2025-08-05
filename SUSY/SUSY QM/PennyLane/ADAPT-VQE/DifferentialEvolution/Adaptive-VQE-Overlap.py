e50d67e15e74a31f68a1fedf3d6e25221caec3f9
JohnKerf <156371414+JohnKerf@users.noreply.github.com>
2025-08-05 13:16:04 +0100

    Run StatevectorOveralp Adaptive-VQE

---
+++ SUSY/SUSY QM/PennyLane/ADAPT-VQE/DifferentialEvolution/Adaptive-VQE-Overlap.py
@@ -0,0 +1,433 @@
+import pennylane as qml
+from pennylane import numpy as pnp
+
+from scipy.optimize import differential_evolution
+from scipy.stats.qmc import Halton
+
+import numpy as np
+from datetime import datetime, timedelta
+
+import os, json, time, logging
+
+from multiprocessing import Pool
+
+from collections import Counter
+
+from susy_qm import calculate_Hamiltonian
+
+
+
+def setup_logger(logfile_path, name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    if not logger.handlers:
+        handler = logging.FileHandler(logfile_path)
+        formatter = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
+
+
+def compute_overlap(params, num_qubits, op_list, basis_state, min_eigenvector):
+
+    dev1 = qml.device("default.qubit", wires=num_qubits, shots=None)
+    @qml.qnode(dev1)
+    def overlap_circuit(params, op_list):
+
+        qml.BasisState(basis_state, wires=range(num_qubits))
+
+        param_index = 0
+        for op in op_list:
+            o = type(op)
+            o(params[param_index], wires=op.wires)
+            param_index +=1
+
+        return qml.state()
+
+    state = overlap_circuit(params, op_list)
+    overlap = np.vdot(min_eigenvector, state)
+    fidelity = np.abs(overlap)**2
+    
+    return fidelity
+
+
+def compute_grad(param, H, num_qubits, operator_ham, op_list, op_params, basis_state):
+
+    dev2 = qml.device("default.qubit", wires=num_qubits, shots=None)
+    @qml.qnode(dev2)
+    def grad_circuit(param, operator_ham, op_list, op_params):
+
+        qml.BasisState(basis_state, wires=range(num_qubits))
+
+        param_index = 0
+        for op in op_list:
+            o = type(op)
+            o(op_params[param_index], wires=op.wires)
+            param_index +=1
+
+        oph = type(operator_ham)
+        oph(param, wires=operator_ham.wires)
+
+        return qml.expval(qml.Hermitian(H, wires=range(num_qubits)))
+    
+    params = pnp.tensor(param, requires_grad=True)
+    grad_fn = qml.grad(grad_circuit)
+    grad = grad_fn(params, operator_ham, op_list, op_params)
+    
+    return grad
+
+
+
+def cost_function(params, H, num_qubits, shots, op_list, basis_state):
+   
+    dev3 = qml.device("default.qubit", wires=num_qubits, shots=shots)
+    start = datetime.now()
+  
+    @qml.qnode(dev3)
+    def circuit(params):
+
+        qml.BasisState(basis_state, wires=range(num_qubits))
+
+        param_index = 0
+        for op in op_list:
+            o = type(op)
+            o(params[param_index], wires=op.wires)
+            param_index +=1
+
+        return qml.expval(qml.Hermitian(H, wires=range(num_qubits)))
+
+     
+    end = datetime.now()
+    device_time = (end - start)
+
+    return circuit(params), device_time
+
+
+def run_adapt_vqe(i, max_iter, tol, H, num_qubits, shots, num_steps, phi, num_grad_checks, operator_pool, basis_state, min_eigenvalue, min_eigenvector, log_dir, strategy, abs_tol, popsize):
+
+    os.makedirs(log_dir, exist_ok=True)
+    log_path = os.path.join(log_dir, f"vqe_run_{i}.log")
+    logger = setup_logger(log_path, f"logger_{i}")
+
+    # We need to generate a random seed for each process otherwise each parallelised run will have the same result
+    seed = (os.getpid() * int(time.time())) % 123456789
+    run_start = datetime.now()
+
+    device_time = timedelta()
+
+    def wrapped_cost_function(params):
+        result, dt = cost_function(params, H, num_qubits, shots, op_list, basis_state)
+        nonlocal device_time
+        device_time += dt
+        return result
+
+    logger.info(f"Starting VQE run {i} (seed={seed})")
+    
+    # Main ADAPT-VQE script
+    op_list = []
+    op_params = []
+    energies = []
+    overlaps = []
+
+    pool = operator_pool.copy()
+    success = False
+    previous_overlap = 0.0
+
+    for i in range(num_steps):
+
+        max_ops_list = []
+        
+        if i != 0:
+            
+            pool.remove(most_common_gate)
+
+            if (type(most_common_gate) == qml.SingleExcitation) or (type(most_common_gate) == qml.CRY):
+                cq = most_common_gate.wires[0]
+                tq = most_common_gate.wires[1]
+
+                if (qml.RY(phi, wires=cq) not in pool):
+                    pool.append(qml.RY(phi, wires=cq))
+
+                if (qml.RZ(phi, wires=cq) not in pool):
+                    pool.append(qml.RZ(phi, wires=cq))
+
+                if (qml.RX(phi, wires=cq) not in pool):
+                    pool.append(qml.RX(phi, wires=cq))
+
+
+
+                if (qml.RY(phi, wires=tq) not in pool):
+                    pool.append(qml.RY(phi, wires=tq))
+
+                if (qml.RZ(phi, wires=tq) not in pool):
+                    pool.append(qml.RZ(phi, wires=tq))
+
+                if (qml.RX(phi, wires=tq) not in pool):
+                    pool.append(qml.RX(phi, wires=tq))
+        
+        for param in np.random.uniform(phi, phi, size=num_grad_checks):
+            grad_list = []
+            for op in pool:
+                grad = compute_grad(param, H, num_qubits, op, op_list, op_params, basis_state)
+                o=type(op)
+
+                if (o == qml.CNOT) or (o == qml.CZ):
+                    grad_op = o(wires=op.wires)
+                else:
+                    grad_op = o(param, wires=op.wires)
+
+                grad_list.append((grad_op,abs(grad)))
+
+            max_op, max_grad = max(grad_list, key=lambda x: x[1])
+            max_ops_list.append(max_op)
+
+        counter = Counter(max_ops_list)
+        most_common_gate, count = counter.most_common(1)[0]
+        op_list.append(most_common_gate)
+
+        # Generate Halton sequence
+        num_dimensions = len(op_list)
+        num_samples = popsize
+        halton_sampler = Halton(d=num_dimensions, seed=seed)
+        halton_samples = halton_sampler.random(n=num_samples)
+        scaled_samples = 2 * np.pi * halton_samples
+
+        bounds = [(0, 2 * np.pi) for _ in range(num_dimensions)]
+        x0 = np.concatenate((op_params, np.array([np.random.random()*2*np.pi])))
+        
+        res = differential_evolution(wrapped_cost_function,
+                                        bounds,
+                                        x0=x0,
+                                        maxiter=max_iter,
+                                        tol=tol,
+                                        atol=abs_tol,
+                                        strategy=strategy,
+                                        popsize=popsize,
+                                        init=scaled_samples,
+                                        seed=seed
+                                        )
+
+        if i!=0: pre_min_e = min_e
+        
+        update = True
+        overlap = compute_overlap(res.x, num_qubits, op_list, basis_state, min_eigenvector)
+
+        if overlap > previous_overlap:
+            logger.info(f"Adding {most_common_gate} increased overlap from {previous_overlap} to {overlap}")
+            previous_overlap = overlap    
+        elif np.abs(overlap - previous_overlap) < 1e-2: 
+            logger.info(f"Adding {most_common_gate} had small change in overlap from {previous_overlap} to {overlap}")
+            previous_overlap = overlap
+        else:
+            logger.info(f"Adding {most_common_gate} decreased overlap from {previous_overlap} to {overlap}.. skipping this gate")
+            #logger.info(f"previous gate list: {op_list}")
+            op_list = op_list[:-1]
+            #logger.info(f"updated gate list: {op_list}")
+            update = False
+        
+        if update:
+            overlaps.append(overlap)
+            min_e = res.fun
+            pre_op_params = op_params.copy()
+            op_params = res.x
+            energies.append(min_e)
+
+
+        if i!=0:
+            if abs(pre_min_e - min_e) < 1e-8:
+                logger.info("gradient converged")
+                if update:
+                    #logger.info(op_list)
+                    energies.pop()
+                    op_list.pop()
+                    overlaps.pop()
+                    final_params = pre_op_params
+                else: 
+                    final_params = op_params
+                success = True
+                break
+            if abs(min_eigenvalue-min_e) < 1e-6:
+                logger.info(f"Converged to min eigenvalue: {min_eigenvalue}")
+                success = True
+                final_params = op_params
+                break
+        
+    run_end = datetime.now()
+    run_time = run_end - run_start
+
+    if success == False:
+        final_params = op_params
+
+    final_ops = []
+    for op, param in zip(op_list,final_params):
+        dict = {"name": op.name,
+                "param": param,
+                "wires": op.wires.tolist()}
+        final_ops.append(dict)
+
+    logger.info(f"Completed VQE run {i}")
+    logger.info(f"Final ops list: {final_ops}")
+    logger.info(f"Energy: {min_e}")
+
+    return {
+        "seed": seed,
+        "energies": energies,
+        "overlaps": overlaps,
+        "min_energy": min_e,
+        #"op_params": op_params,
+        "op_list": final_ops,
+        "success": success,
+        "num_iters": i+1,
+        "run_time": run_time,
+        "device_time": device_time
+    }
+
+
+if __name__ == "__main__":
+    
+    potential = "DW"
+    cutoff_list = [2,4,8,16,32,64]
+    shots = None
+
+    for cutoff in cutoff_list:
+
+        print(f"Running for {potential} potential, cutoff {cutoff}")
+
+        starttime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        base_path = os.path.join(r"/users/johnkerf/Quantum Computing/SUSY-QM/PennyLane/ADAPT-VQE/DifferentialEvolution/StatevectorFiles-Overlap", potential)
+        log_path = os.path.join(base_path, f"logs_{str(cutoff)}")
+        os.makedirs(log_path, exist_ok=True)
+
+
+        # Calculate Hamiltonian and expected eigenvalues
+        H = calculate_Hamiltonian(cutoff, potential)
+        eigenvalues = np.sort(np.linalg.eig(H)[0])[:4]
+        min_eigenvalue = np.min(eigenvalues)
+
+        
+        eiv, eigenvectors = np.linalg.eig(H)
+        min_3_ev = eiv.argsort()[:4]
+        min_eigenvector = np.asarray(eigenvectors[:, min_3_ev[0]])
+        min_eigenvalue = min_3_ev[0]
+
+
+        num_qubits = int(1 + np.log2(cutoff))
+
+        #Create operator pool
+        operator_pool = []
+        phi = 0.0
+        for i in range(num_qubits):
+            operator_pool.append(qml.RY(phi,wires=[i]))
+            #operator_pool.append(qml.RZ(phi,wires=[i]))
+            #operator_pool.append(qml.RX(phi,wires=[i]))
+
+        c_pool = []
+
+        for control in range(num_qubits):
+                for target in range(num_qubits):
+                    if control != target:
+                        c_pool.append(qml.CRY(phi=phi, wires=[control, target]))
+                        #c_pool.append(qml.SingleExcitation(phi, wires=[control, target]))
+
+        operator_pool = operator_pool + c_pool    
+
+        # Choose basis state
+        if potential == 'DW':
+            if cutoff == 4:
+                basis_state = [1] + [0]*(num_qubits-1)
+            else:
+                basis_state = [0]*(num_qubits)
+        else:
+            basis_state = [1] + [0]*(num_qubits-1)
+        
+        
+
+        # Optimizer
+        num_steps = 30
+        num_grad_checks = 1
+        num_vqe_runs = 100
+        max_iter = 10000
+        tol = 1e-8
+
+        # Optimizer
+        num_steps = 30
+        num_grad_checks = 1
+        num_vqe_runs = 100
+        max_iter = 500
+        strategy = "randtobest1bin"
+        tol = 1e-3
+        abs_tol = 1e-2
+        popsize = 20
+
+
+        vqe_starttime = datetime.now()
+
+        print("Starting ADAPT-VQE")
+        # Start multiprocessing for VQE runs
+        with Pool(processes=100) as pool:
+            vqe_results = pool.starmap(
+                run_adapt_vqe,
+                [
+                    (i, max_iter, tol, H, num_qubits, shots, num_steps, phi, num_grad_checks, operator_pool, basis_state, min_eigenvalue, min_eigenvector, log_path, strategy, abs_tol, popsize)
+                    for i in range(num_vqe_runs)
+                ],
+            )
+
+        print("Finished ADAPT-VQE")
+        # Collect results
+        seeds = [res["seed"] for res in vqe_results]
+        all_energies = [res["energies"] for res in vqe_results]
+        all_overlaps = [res["overlaps"] for res in vqe_results]
+        min_energies = [res["min_energy"] for res in vqe_results]
+        #op_params = [str(res["op_params"]) for res in vqe_results]
+        op_lists = [res["op_list"] for res in vqe_results]
+        success = [res["success"] for res in vqe_results]
+        num_iters = [res["num_iters"] for res in vqe_results]
+        run_times = [str(res["run_time"]) for res in vqe_results]
+        total_run_time = sum([res["run_time"] for res in vqe_results], timedelta())
+        total_device_time = sum([res['device_time'] for res in vqe_results], timedelta())
+
+        vqe_end = datetime.now()
+        vqe_time = vqe_end - vqe_starttime
+
+        # Save run
+        run = {
+            "starttime": starttime,
+            "endtime": vqe_end.strftime("%Y-%m-%d_%H-%M-%S"),
+            "potential": potential,
+            "cutoff": cutoff,
+            "exact_eigenvalues": [x.real.tolist() for x in eigenvalues],
+            "ansatz": "circuit.txt",
+            "shots": shots,
+            "Optimizer": {
+                "name": "COBYLA",
+                "bounds": "[(0, 2 * np.pi)]",
+                "maxiter": max_iter,
+                "tolerance": tol
+            },
+            "num_VQE": num_vqe_runs,
+            "num_steps":num_steps,
+            "num_grad_checks":num_grad_checks,
+            "phi": phi,
+            "basis_state": basis_state,
+            "operator_pool": [str(op) for op in operator_pool],
+            "all_energies": all_energies,
+            "all_overlaps": all_overlaps,
+            "min_energies": min_energies,
+            #"op_params": op_params,
+            "op_list": op_lists,
+            "num_iters": num_iters,
+            "success": np.array(success, dtype=bool).tolist(),
+            "run_times": run_times,
+            "seeds": seeds,
+            "parallel_run_time": str(vqe_time),
+            "total_VQE_time": str(total_run_time),
+            "total_device_time": str(total_device_time)
+        }
+
+        # Save the variable to a JSON file
+        path = os.path.join(base_path, "{}_{}.json".format(potential, cutoff))
+        with open(path, "w") as json_file:
+            json.dump(run, json_file, indent=4)
+
+        print("Done")
